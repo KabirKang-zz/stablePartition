4c7c808ee3b4f0ac3e27f2a4f7bf0e6e2b7a1d20
Kabir Kang <kangkabir@gmail.com>
2015-05-01 12:04:43 -0700

    Complete implementation

--- partitioner.java
+++ partitioner.java
@@ -21,17 +21,37 @@ public class partitioner {
 
 
 		while(n!=null){
+			LinkedListNode next = n.next;
+			n.next = null;
 			if(n.data<x){
-				
+				if(beforeStart == null){
+					beforeStart = n;
+					beforeEnd = beforeStart;
+				}
+				else{
+					beforeEnd.next = n;
+					beforeEnd = n;
+				}
 			}
 			else{
-				
+				if(afterStart == null){
+					afterStart = n;
+					afterEnd = afterStart;
+				}
+				else{
+					afterEnd.next = n;
+					afterEnd = n;
+				}
 			}
+			n = next;
 		}
 
 		if(beforeStart == null){
 			return afterStart;
 		}
+
+		beforeEnd.next = afterStart;
+		
 		return beforeStart;
 	}
 
