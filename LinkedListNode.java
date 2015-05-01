27739b380925637af4a9df2e0a21be8a5d511265
Kabir Kang <kangkabir@gmail.com>
2015-05-01 11:21:30 -0700

    Skeleton for stable linked list partition

---
+++ LinkedListNode.java
@@ -0,0 +1,10 @@
+package partitionStable;
+
+public class LinkedListNode {
+	public LinkedListNode next;
+	public int data;
+
+	public LinkedListNode(int x){
+		data = x;
+	}
+}
---
+++ partitioner.java
@@ -0,0 +1,8 @@
+package partitionStable;
+
+public class partitioner {
+
+	public static void main(String[] args){
+		System.out.println("Welcome to the stable partitioner.");		
+	}
+}
